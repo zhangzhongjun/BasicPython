ca3a9ca3e3b3c6d98a8142930361c40092334af1
张中俊 <1014830422@qq.com>
2018-08-17 16:58:58 +0800

    适配了python3的异常处理机制写法

--- 异常.py
+++ 异常.py
@@ -11,8 +11,9 @@ else:
 try:
     fh = open("testfile", "w")
     fh.write("这是一个测试文件，用于测试异常!!")
-except IOError:
+except IOError as e:
     print "Error: 没有找到文件或读取文件失败"
+    print(e.reason)
 else:
     print "内容写入文件成功"
     fh.close()
